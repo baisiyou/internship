a69b8d88e3275f6b3034ae62337e135615b8287f
Rubing Zhang <69253088+baisiyou@users.noreply.github.com>
2024-04-11 18:47:28 -0400

    "

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -6,3 +6,23 @@ test('renders without crashing', () => {
   const { unmount } = render(<App />);
   unmount();
 });
+
+test('renders without crashing 2', () => {
+  const { unmount } = render(<App />);
+  unmount();
+});
+
+test('renders without crashing 3', () => {
+  const { unmount } = render(<App />);
+  unmount();
+});
+
+test('renders without crashing 4', () => {
+  const { unmount } = render(<App />);
+  unmount();
+});
+
+test('renders without crashing 5', () => {
+  const { unmount } = render(<App />);
+  unmount();
+});
